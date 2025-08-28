a22f82fa85535e13d81af56fbb348d1c5785549a
Breao <1792083+Breao@users.noreply.github.com>
2025-08-27 20:25:10 -1000

    Formating

--- components/js/main.js
+++ components/js/main.js
@@ -2,21 +2,21 @@ const DEBUG = true;
 
 
 function loadHTML(elem, source) {
-    if(DEBUG) {
-        console.log("loading HTML");
-        console.log("element", elem);
-        console.log("source", source);
-    }
-    fetch(source + '.html')
-        .then(response => response.text())
-        .then(text => document.getElementById(elem).innerHTML = text);
-  } 
+  if (DEBUG) {
+    console.log("loading HTML");
+    console.log("element", elem);
+    console.log("source", source);
+  }
+  fetch(source + '.html')
+    .then(response => response.text())
+    .then(text => document.getElementById(elem).innerHTML = text);
+}
 
 const getJSON = async url => {
-    const response = await fetch(url);
-    if(!response.ok) // check if response worked (no 404 errors etc...)
-      throw new Error(response.statusText);
-  
-    const data = response.json(); // get JSON from the response
-    return data; // returns a promise, which resolves to this data value
-  }
+  const response = await fetch(url);
+  if (!response.ok) // check if response worked (no 404 errors etc...)
+    throw new Error(response.statusText);
+
+  const data = response.json(); // get JSON from the response
+  return data; // returns a promise, which resolves to this data value
+}
